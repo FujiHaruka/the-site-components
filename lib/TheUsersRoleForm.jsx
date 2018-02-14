f5e6486e3a7a8bb494fd9766f5c87626979bb431
okunishinishi <okunishinishi@gmail.com>
2018-02-14 13:32:38 +0900

    Releasing new package

---
+++ lib/TheUsersRoleForm.jsx
@@ -0,0 +1,59 @@
+/**
+ * TheUsersRoleForm component
+ */
+'use strict'
+
+import React from 'react'
+import { TheButton, TheButtonGroup, TheForm, TheInput } from 'the-components'
+
+const {Radio, Text} = TheInput
+const {Field, Label, Value} = TheForm
+
+function TheUsersRoleForm ({
+                             getFormAttributes,
+                             getInputAttributesOf,
+                             getLabelAttributesOf,
+                             getSubmitAttributes,
+                             l,
+                             users = [],
+                             roles = {},
+                           }) {
+  return (
+    <TheForm {...getFormAttributes()}
+             autoComplete='off'
+             required={['role']}
+    >
+      <Field>
+        <Label {...getLabelAttributesOf('targets')}>
+          {l('labels.TARGETS')}
+        </Label>
+        <Value>
+          {users?.map((user) => (
+            <li key={user.id}>
+              <span>{user.displayName}</span>
+              <span>{user.role?.code && ` ( ${user.role?.code} ) `}</span>
+            </li>
+          ))}
+        </Value>
+      </Field>
+      <Field>
+        <Label {...getLabelAttributesOf('role')}>
+          {l('labels.USER_ROLE')}
+        </Label>
+        <Value>
+          <Radio {...getInputAttributesOf('role')}
+                 options={roles}
+          />
+        </Value>
+      </Field>
+      <br/>
+      <Field>
+        <TheButton primary wide {...getSubmitAttributes()}>
+          {l('buttons.DO_UPDATE')}
+        </TheButton>
+      </Field>
+    </TheForm>
+  )
+}
+
+export default TheUsersRoleForm
